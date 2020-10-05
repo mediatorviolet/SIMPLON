b38cabde0e4976ec7974260b6ab7cbe09411d0ae
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-10-05 09:35:34 +0400

    .

--- todoapp/js/script.js
+++ todoapp/js/script.js
@@ -1,7 +1,7 @@
 function check() {
     var checkbox = document.getElementsByClassName("myCheck");
-    var lineThrough = document.getElementsByClassName("myList");
-    for (var i = 0; i < lineThrough.length; i++){
+    var lineThrough = document.getElementsByClassName("myItem");
+    for (var i = 0; i < lineThrough.length; i++) {
         if (checkbox[i].checked == true) {
             lineThrough[i].style.textDecoration = "line-through";
         } else {
@@ -18,12 +18,23 @@ function check() {
     document.URL.append(newLi.appendChild(textTask));
 }*/
 
-function addTask(){
-    var newTask = document.getElementById('newTask');
-    var addBtn = document.getElementById('addBtn');
-    if (addBtn.clicked == true && newTask != ''){
-        
+function addTask() {
+    var newTask = document.getElementById('newTask').value;
+    var ul = document.getElementById('myList');
+    var li = document.createElement('li');
+    li.className = 'myItem';
+    var input = document.createElement('input');
+    input.className = 'mr-3 myCheck';
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('onclick', 'check()');
+    if (newTask != '') {
+        li.appendChild(input);
+        li.appendChild(document.createTextNode(newTask));
+        ul.appendChild(li);
     }
 }
+
+var addBtn = document.getElementById('addBtn');
+addBtn.addEventListener('click', addTask);
 
 // idée => quand clicked important va en haut de la liste (:
