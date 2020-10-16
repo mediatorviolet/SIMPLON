a14d937522fb03aa800b57493f96c3e8572c0426
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2020-10-16 13:13:26 +0400

    validation form

--- TP Formulaire/form.js
+++ TP Formulaire/form.js
@@ -1,8 +1,14 @@
-var email = document.getElementById('email');
-email.addEventListener('keyup', function(event) {
-    if (email.validity.typeMismatch) {
-        email.setCustomValidity('EMAAAAAAAIL');
-    } else {
-        email.setCustomValidity('');
+var formValid = document.getElementById("submitBtn");
+var firstName = document.getElementById('firstName');
+var lastName  = document.getElementById('lastName');
+var validName = /^[a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+([-'\s][a-zA-ZéèîïÉÈÎÏ][a-zéèêàçîï]+)?$/;
+formValid.addEventListener('click', validation);
+
+function validation(event) {
+    if (firstName.value == '' || lastName.value == '') {
+        event.preventDefault();
+        firstName.setCustomValidity('Please enter your fisrt name.');
+    } else if (validName.test(firstName.value) == false || validName.test(lastName.value) == false) {
+        event.preventDefault();
     }
-});
+}
